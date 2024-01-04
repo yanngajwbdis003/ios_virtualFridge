0debf5c286b89875f419c1d859d87c492fd36542
mo <yanngajwbdis@gmail.com>
2024-01-04 13:46:49 -0800

    Initial Commit

---
+++ virtualFridge/ViewModel/MainViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewModel.swift
+//  virtualFridge
+//
+//  Created by Monica Yang on 2024-01-04.
+//
+
+import SwiftUI
+
+struct MainViewModel: View {
+    var body: some View {
+        Text("ho0, World!")
+    }
+}
+
+#Preview {
+    MainViewModel()
+}
